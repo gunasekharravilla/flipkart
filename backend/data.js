890e7d7ffdaca65948dc57c71ea3cd55fa0c0eb6
gunasekharravilla <gunasekharravilla156@gmail.com>
2020-12-09 12:09:11 +0530

    Frontend

---
+++ backend/data.js
@@ -0,0 +1,83 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12899750/2020/11/23/8406a90d-923a-4d67-95ad-c7ae8aff12e41606111228375-Puma-Men-Sweatshirts-8101606111226506-1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "2",
+      name: "Adidas Fit Shirt",
+      category: "Shirts",
+      image:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12899750/2020/11/23/8406a90d-923a-4d67-95ad-c7ae8aff12e41606111228375-Puma-Men-Sweatshirts-8101606111226506-1.jpg",
+      price: 100,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "3",
+      name: "Lacoste Free Shirt",
+      category: "Shirts",
+      image:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12899750/2020/11/23/8406a90d-923a-4d67-95ad-c7ae8aff12e41606111228375-Puma-Men-Sweatshirts-8101606111226506-1.jpg",
+      price: 220,
+      countInStock: 20,
+      brand: "Lacoste",
+      rating: 4.8,
+      numReviews: 17,
+      description: "high quality product",
+    },
+    {
+      _id: "4",
+      name: "Nike Slim Pant",
+      category: "Pants",
+      image:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12899750/2020/11/23/8406a90d-923a-4d67-95ad-c7ae8aff12e41606111228375-Puma-Men-Sweatshirts-8101606111226506-1.jpg",
+      price: 78,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality product",
+    },
+    {
+      _id: "5",
+      name: "Puma Slim Pant",
+      category: "Pants",
+      image:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12899750/2020/11/23/8406a90d-923a-4d67-95ad-c7ae8aff12e41606111228375-Puma-Men-Sweatshirts-8101606111226506-1.jpg",
+      price: 6500,
+      countInStock: 5,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "6",
+      name: "Adidas Fit Pant",
+      category: "Pants",
+      image:
+        "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12899750/2020/11/23/8406a90d-923a-4d67-95ad-c7ae8aff12e41606111228375-Puma-Men-Sweatshirts-8101606111226506-1.jpg",
+      price: 13999,
+      countInStock: 12,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 15,
+      description: "high quality product",
+    },
+  ],
+};
+export default data;
