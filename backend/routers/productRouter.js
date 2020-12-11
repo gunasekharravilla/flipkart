90333de159ff3a23e82eaf88bc0c3ae6990d5ac3
gunasekharravilla <gunasekharravilla156@gmail.com>
2020-12-11 22:50:38 +0530

    master

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -40,7 +40,7 @@ productRouter.post(
   isAdmin,
   expressAsyncHandler(async (req, res) => {
     const product = new Product({
-      name: "sample name " + Date.now(),
+      name: "sample name " + Date.UTC(),
       image: "/images/p1.jpg",
       price: 0,
       category: "sample category",
@@ -52,6 +52,32 @@ productRouter.post(
     });
     const createdProduct = await product.save();
     res.send({ message: "Product Created", product: createdProduct });
+  })
+);
+
+productRouter.put(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name;
+      product.price = req.body.price;
+      product.image = req.body.image;
+      product.category = req.body.category;
+      product.brand = req.body.brand;
+      product.countInStock = req.body.countInStock;
+      product.description = req.body.description;
+      const updatedProduct = await product.save();
+      res.send({
+        message: "Product Updated Successfully !!",
+        product: updatedProduct,
+      });
+    } else {
+      res.status(404).send({ message: "Product Not Found" });
+    }
   })
 );
 
